09fc41f665dd9f9da37e2c14c0aec047fb4b7876
digitalmonk08 <144095649+digitalmonk08@users.noreply.github.com>
2023-10-26 17:40:12 +0530

    ads

--- index.php
+++ index.php
@@ -17,7 +17,8 @@
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
      echo ($row['customer_id'] . " " . $row['email'] . PHP_EOL);
     }
-   //$tsgl = " INSERT INTO kareena VALUES VALUES ('54', '5677', 'testw@gmail.com', 'hello', 'hello', '26-10-23');";
+   //$tsgl = " INSERT INTO kareena  
+   //abvVALUES ('54', '5677', 'testw@gmail.com', 'hello', 'hello', '26-10-23');";
 
     sqlsrv_free_stmt($getResults);
 ?>
