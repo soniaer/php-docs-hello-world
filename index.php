ddaf3fde721c8988d6599cca63589cc7ad669b31
digitalmonk08 <144095649+digitalmonk08@users.noreply.github.com>
2023-10-26 18:21:57 +0530

    aaa

--- index.php
+++ index.php
@@ -9,23 +9,23 @@
     $value = $_GET["name"];
     echo "name";
     echo ($value);
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT * FROM kareena";
-    $getResults= sqlsrv_query($conn, $tsql);
-    echo ($conn);
-    echo ($getResults);
-    echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-echo ($row['customer_id'] . " " . $row['email'] . " " . $row['incoming_msg'] . " " . $row['AI_msg'] .  PHP_EOL);
-    }
+//     $conn = sqlsrv_connect($serverName, $connectionOptions);
+//     $tsql= "SELECT * FROM kareena";
+//     $getResults= sqlsrv_query($conn, $tsql);
+//     echo ($conn);
+//     echo ($getResults);
+//     echo ("Reading data from table" . PHP_EOL);
+//     if ($getResults == FALSE)
+//         echo (sqlsrv_errors());
+//     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+// echo ($row['customer_id'] . " " . $row['email'] . " " . $row['incoming_msg'] . " " . $row['AI_msg'] .  PHP_EOL);
+//     }
 
-    $tmgl = "INSERT INTO kareena (customer_id, email, incoming_msg, AI_msg, category, mytimestamp)
-   VALUES ('54', 'testw@gmail.com', 'hello', 'hii', 'clinical', '26-10-23')";
+//     $tmgl = "INSERT INTO kareena (customer_id, email, incoming_msg, AI_msg, category, mytimestamp)
+//    VALUES ('54', 'testw@gmail.com', 'hello', 'hii', 'clinical', '26-10-23')";
 
- $getResult= sqlsrv_query($conn, $tmgl);
+//  $getResult= sqlsrv_query($conn, $tmgl);
 
-    sqlsrv_free_stmt($getResults);
-    sqlsrv_free_stmt($getResult);
+//     sqlsrv_free_stmt($getResults);
+//     sqlsrv_free_stmt($getResult);
 ?>
